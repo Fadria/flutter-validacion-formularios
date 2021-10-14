f7e7bed8f5d4bd589b4c8f2f0976a3eddcc010d0
Federico Adria <fadriacarrasco@gmail.com>
2021-10-15 00:08:18 +0200

    Formulario de Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,13 +24,49 @@ class LoginScreen extends StatelessWidget {
                   Text("Login", style: Theme.of(context).textTheme.headline4),
                   SizedBox(height: 30),
 
-                  Text("Mi formulario")
+                  _LoginForm(),
                 ])
               ),
+
+              SizedBox(height: 50),
+              Text("Crear una nueva cuenta", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             ],
           ),
         )
       )
    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container
+    (
+      child: Form
+      (
+        // TO DO: mantener la referencia al KEY
+        child: Column
+        (
+          children: 
+          [
+            TextFormField
+            (
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration
+              (
+                enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple.shade800)),
+                focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.deepPurple.shade800, width: 2)),
+                hintText: "fadriacarrasco@gmail.com",
+                labelText: "Email",
+                labelStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple.shade800)
+              ),
+            )
+          ],
+        )
+      ),
+    );
   }
 }
